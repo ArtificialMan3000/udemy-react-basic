34b557e0a1eda524efdb393636461cff99f66d7e
Aleksandr <legionerab3000@yandex.ru>
2021-12-08 17:21:37 +0300

    Convert form to controlled component

--- src/components/employees-add-form/employees-add-form.jsx
+++ src/components/employees-add-form/employees-add-form.jsx
@@ -1,29 +1,52 @@
 import React from 'react';
 import './employees-add-form.css';
 
-export const EmployeesAddForm = () => {
-  return (
-    <div className="app-add-form">
-      <h3>Добавьте нового сотрудника</h3>
-      <form className="add-form d-flex">
-        <input
-          type="text"
-          className="form-control new-post-label"
-          placeholder="Как его зовут?"
-        />
-        <input
-          type="number"
-          className="form-control new-post-label"
-          placeholder="З/П в $?"
-        />
+export class EmployeesAddForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      salary: 0,
+    };
+  }
 
-        <button
-          type="submit"
-          className="btn btn-outline-light"
-        >
-          Добавить
-        </button>
-      </form>
-    </div>
-  );
-};
+  onValueChange = (evt) => {
+    this.setState({
+      [evt.target.name]: evt.target.value,
+    });
+  };
+
+  render() {
+    const { name, salary } = this.state;
+    return (
+      <div className="app-add-form">
+        <h3>Добавьте нового сотрудника</h3>
+        <form className="add-form d-flex">
+          <input
+            type="text"
+            className="form-control new-post-label"
+            placeholder="Как его зовут?"
+            name="name"
+            value={name}
+            onChange={this.onValueChange}
+          />
+          <input
+            type="number"
+            className="form-control new-post-label"
+            placeholder="З/П в $?"
+            name="salary"
+            value={salary}
+            onChange={this.onValueChange}
+          />
+
+          <button
+            type="submit"
+            className="btn btn-outline-light"
+          >
+            Добавить
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
